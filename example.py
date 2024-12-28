4f800edcf819613a667d5916815db811c3bc4f40
@LAKA <lakalakazy99@gmail.com>
2024-12-28 09:51:42 +0530

    example.py

---
+++ example.py
@@ -0,0 +1,13 @@
+# lobby.py
+BLUE = '\033[94m'
+RESET = '\033[0m'  
+
+def welcome_message():
+    print(f"{BLUE}NO SYSTEM IS SAFE{RESET}")
+    print(f"{BLUE}Tool created by LAKA.{RESET}")
+
+def main():
+    welcome_message()
+
+if __name__ == "__main__":
+    main()
