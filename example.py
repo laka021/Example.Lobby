570a6fc2ff3ed14b1ca0852a8ed74460b2069cf6
@LAKA <lakalakazy99@gmail.com>
2024-12-28 10:09:34 +0530

    example.py

--- example.py
+++ example.py
@@ -1,13 +1,35 @@
 # lobby.py
-BLUE = '\033[94m'
-RESET = '\033[0m'  
 
-def welcome_message():
-    print(f"{BLUE}NO SYSTEM IS SAFE{RESET}")
-    print(f"{BLUE}Tool created by LAKA.{RESET}")
+import time
+import sys
+import pyfiglet
+
+BLUE = '\033[94m'
+RESET = '\033[0m'
+
+def animated_welcome_message():
+    message = "NO SYSTEM IS SAFE"
+    tool_creator = "Tool created by LAKA."
+
+    ascii_art = pyfiglet.figlet_format(message)
+    
+    for char in ascii_art:
+        sys.stdout.write(f"{BLUE}{char}{RESET}")
+        sys.stdout.flush()
+        time.sleep(0.05)  
+    
+    time.sleep(0.5)  
+
+    print()  
+    for char in tool_creator:
+        sys.stdout.write(f"{BLUE}{char}{RESET}")
+        sys.stdout.flush()
+        time.sleep(0.1) 
+    
+    print()  
 
 def main():
-    welcome_message()
-
+    animated_welcome_message()
+   
 if __name__ == "__main__":
     main()
